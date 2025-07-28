3966faed99b34c01fe22f535606f554a00522c0a
Nishanthsvbhat <nishanthsvnsv@example.com>
2025-07-28 18:52:15 +0530

    Update

--- backend/models.py
+++ backend/models.py
@@ -1,24 +1,16 @@
+# backend/models.py
+from pymongo import MongoClient
 import os
-from flask import Flask
-from flask_pymongo import PyMongo
-from dotenv import load_dotenv
 
-load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")  # Get from Render Env Var
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["incidentDB"]
+collection = db["incidents"]
 
-mongo = PyMongo(app)
-incident_collection = mongo.db.client.get_database().get_collection("incidents")
-
-def log_incident(title, description):
-    incident = {
-        "title": title,
-        "description": description
-    }
-    incident_collection.insert_one(incident)
+def add_incident(title, description):
+    collection.insert_one({"title": title, "description": description})
 
 def get_all_incidents():
-    incidents = incident_collection.find()
-    print(incidents)
+    incidents = collection.find()
     return [{"title": inc["title"], "description": inc["description"]} for inc in incidents]
