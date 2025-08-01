daf6352159993ba23a09500706c801f2a2088d3e
Nishanth S V <166023261+nishanthsvbhat@users.noreply.github.com>
2025-08-01 18:17:12 +0530

    db.py

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,22 @@
-# backend/db.py
-
+from flask import Flask
 from flask_pymongo import PyMongo
 
-# Create a PyMongo instance that we can initialize later in our main app
-mongo = PyMongo()
+def initialize_mongo(app:Flask):
+    """
+    Initialize the PyMongo instance with the Flask app.
+    """
+    mongo = PyMongo(app)
+    return mongo
 
-def ensure_collections():
-    # Explicitly create 'incidents' collection if it doesn't exist
+def ensure_collections(mongo: PyMongo):
+    """
+    Ensure that the required collections exist in the MongoDB database.
+    """
+    if not mongo:
+        raise ValueError("Mongo instance is not initialized")
+    if mongo.db is None:
+        raise ValueError("Mongo database is not initialized")
+    # Check if the 'incidents' collection exists, if not create it
     existing_collections = mongo.db.list_collection_names()
     if "incidents" not in existing_collections:
         mongo.db.create_collection("incidents")
